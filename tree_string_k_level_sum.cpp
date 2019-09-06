c2f3fe0fcee99c88e38c7b1eb42c4e94fe1ee689
Mohd-Saqib9718 <47239666+Mohd-Saqib9718@users.noreply.github.com>
2019-09-06 21:03:30 +0530

    Create tree_string_k_level_sum.cpp

---
+++ tree_string_k_level_sum.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+// Function to find sum of digits 
+// of elements at k-th level 
+int sumAtKthLevel(string tree, int k) 
+{ 
+    int level = -1; 
+    int sum = 0;  // Initialize result 
+    int n = tree.length(); 
+  
+    for (int i=0; i<n; i++) 
+    { 
+        // increasing level number 
+        if (tree[i] == '(') 
+            level++; 
+  
+        // decreasing level number 
+        else if (tree[i] == ')') 
+            level--; 
+  
+        else
+        { 
+            // check if current level is 
+            // the desired level or not 
+            if (level == k) 
+                sum += (tree[i]-'0'); 
+        } 
+    } 
+  
+    // required sum 
+    return sum; 
+} 
+  
+// Driver program to test above 
+int main() 
+{ 
+    string tree = "(0(5(6()())(4()(9()())))(7(1()())(3()())))"; 
+    int k = 2; 
+    cout << sumAtKthLevel(tree, k); 
+    return 0; 
+} 
