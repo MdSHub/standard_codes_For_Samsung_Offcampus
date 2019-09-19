1601fcdfc91e9612313c6d21419f6c0ba8dc8e28
Mohd-Saqib9718 <47239666+Mohd-Saqib9718@users.noreply.github.com>
2019-09-19 10:30:57 +0530

    Update Space_Ship.cpp

--- Space_Ship.cpp
+++ Space_Ship.cpp
@@ -14,56 +14,137 @@ Here the main catch is that the cost to pass through wormhole can be zero so you
 */
 
 
-#include <iostream>
-#define lim 10
-
+#include<iostream>
 using namespace std;
+#define i_max 2147483647
+int nw,sx,sy,dx,dy;//Source and destination co-ordinates
+int dist[1001][1001];
+bool set[1001][1001];
+class Wormhole{
+	public:
+		int x1,y1,x2,y2,cost;
+		
+		void get(){
+			cin >> x1 >> y1 >> x2 >> y2 >> cost;
+		}
+};
+Wormhole A[10];
+int row_mov[] = {1,-1,0,0};
+int col_mov[] = {0,0,1,-1}; 
 
-int visited[lim], wh[lim][5];
-int ans,N;
 
-int abs(int x){
-    if(x>=0)
-        return x;
-    else
-        return -x;
+void initializeArrays(int m,int n){
+	//Filling array with 1s
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			dist[i][j] = i_max;
+			set[i][j] = false;
+		}
+	}
 }
 
-int distance(int sx, int sy, int dx, int dy){
-    return (abs(sx-dx)+abs(sy-dy));
+void setMinRowCol(int &row,int &col){
+	int min_dist=i_max,min_i=-1,min_j=-1;
+	for(int i=1;i<=dx;i++){
+		for(int j=1;j<=dy;j++){
+			if(!set[i][j] && dist[i][j]<min_dist){
+				min_dist = dist[i][j];
+				min_i = i;
+				min_j = j;
+			}
+		}
+	}
+	row = min_i;
+	col = min_j;
 }
 
-void fn(int sx, int sy, int dx, int dy, int dis){
-    ans = min(ans, distance(sx, sy, dx, dy)+dis);
-
-    for(int i=0; i<N; i++){
-        if(visited[i]==0){
-            visited[i]=1;
-            int temp = distance(sx, sy, wh[i][0], wh[i][1])+wh[i][4]+dis;
-            fn(wh[i][2], wh[i][3], dx, dy, temp);
-            temp = distance(sx, sy, wh[i][2], wh[i][3])+wh[i][4]+dis;
-            fn(wh[i][0], wh[i][1], dx, dy, temp);
-            visited[i]=0;
-        }
+int wormentry(int i,int j)
+{
+    for(int k=0;k<nw;k++)
+    {
+        if(A[k].x1==i && A[k].y1==j)
+            return k;
     }
+    return -1;
 }
 
+int wormexit(int i,int j)
+{
+    for(int k=0;k<nw;k++)
+    {
+        if(A[k].x2==i && A[k].y2==j)
+            return k;
+    }
+    return -1;
+}
+
+bool isValid(int i,int j){
+	if(i<1 || j<1 || i>dx || j>dy){
+		return false;
+	}
+	return true;
+}
+
+int findMinCost(){
+	initializeArrays(dx+1,dy+1);
+	//Run dijkstra
+	dist[sx][sy] = 0;//Distance from source to source is 0
+	//set[sx][sy] = true;//We have reached source
+	
+	for(int i=1;i<=dx*dy;i++){
+		int row=-1,col=-1;
+		setMinRowCol(row,col);//Find min row,col that are unset and proceed with min_dist path
+		
+		if(row == -1 && col == -1){//If not found then break
+			break;
+		}
+		if(row == dx && col == dy){//If we've reached destination, return the cost
+			return dist[row][col];
+		}
+		
+		set[row][col] = true;//Set the current row,col
+		
+		for(int j=0;j<4;j++)//Move in all 4 directions
+        {
+            int x = row + row_mov[j];
+            int y = col + col_mov[j];
+
+            if(isValid(x,y) && !set[x][y] && dist[x][y]>dist[row][col]+1)
+                dist[x][y] = dist[row][col] + 1;
+        }
+        
+        int w_entry = wormentry(row,col);//If worm entry is at row,col ,i.e., row = x1 & col = y1 then exit is at x2,y2
+        if(w_entry!=-1)
+        {
+            int x = A[w_entry].x2;
+            int y = A[w_entry].y2;
+
+            if(!set[x][y] && dist[x][y]>dist[row][col]+A[w_entry].cost) //If cost of wormhole is minimum go through the wormhole
+                dist[x][y] = dist[row][col]+A[w_entry].cost;
+        }
+        int w_exit = wormexit(row,col);//If worm exit is at row,col ,i.e., row = x2 & col = y2 then entry is at x1,y1
+        if(w_exit!=-1)
+        {
+            int x = A[w_exit].x1;
+            int y = A[w_exit].y1;
+
+            if(!set[x][y] && dist[x][y]>dist[row][col]+A[w_exit].cost) //If cost of wormhole is minimum go through the wormhole
+                dist[x][y] = dist[row][col]+A[w_exit].cost;
+        }
+	}
+	return -1;
+}
 
 int main(){
-    int t;
-    cin>>t;
-    while(t>0){
-        t--;
-        cin>>N;
-        int sx, sy, dx, dy;
-        cin>>sx>>sy>>dx>>dy;
-        for(int i=0; i<N; i++){
-            visited[i]=0;
-            for(int j=0; j<5; j++){
-                cin>>wh[i][j];
-            }
-        }
-        ans=999999;
-        cout<<ans<<endl;
-    }
+	cin >> sx >> sy >> dx >> dy;//Scanning source and destination
+	
+	cin >> nw;//No of wormholes
+	
+	//Scanning wormholes info
+	for(int i=0;i<nw;i++){
+		A[i].get();
+	}
+	
+	cout << findMinCost();
+	return 0;
 }
