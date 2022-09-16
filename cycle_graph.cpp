4eb08e93c2718e7d26ee7e50ff9012ef8bf7b4b4
Mohd Saqib <saqibali9718620@gmail.com>
2022-09-16 15:29:10 +0530

    Update cycle_graph.cpp

--- cycle_graph.cpp
+++ cycle_graph.cpp
@@ -83,128 +83,150 @@ Output:
 */
 
 
-#include <iostream>
+#include<stdio.h>
 
-using namespace std;
-
-int N, E;//Number of Nodes, Number of edges
-bool V[100][100];
-bool visited[100];
-int k,l;  //The node from where the cycle starts
+int V[100][100];
+int visited[100];
+int N,E;
+int k,l;
 int path[100];
 
+
 void input(){
-    cin>>N>>E;
-    for(int i=0; i<E; i++){
-        int x,y;
-        cin>>x>>y;
-        V[x][y]=true;
-    }
+ scanf("%d",&N);
+ scanf("%d",&E);
+ for(int i=0;i<E;i++){
+    int x,y;
+    scanf("%d",&x);
+    scanf("%d",&y);
+    V[x][y] = 1;
+ }
+
+
 }
+
 
 void reset(){
     for(int i=0; i<100; i++){
         for(int j=0; j<100; j++)
-            V[i][j]=false;
-        visited[i]=false;
-        path[i]=-1;
+            V[i][j]=0;
+            visited[i]=0;
+            path[i]=-1;
     }
+
+
     k=-2;
     l=-2;
+
 }
 
 
-int recur(int node, int counter){
+  int recur(int node, int counter){
 
-    /*cout<<"\n";
-    for(int i=0; i<counter; i++)
-        cout<<"     ";
-    cout<<node;
-    */
-    path[counter]=node;
+      path[counter]= node;
 
-    for(int y=0; y<N; y++){
-        if(V[node][y]){
-            if(visited[y]){
+      for(int y=0;y<N;y++){
+        if(V[node][y] == 1){
+            if(visited[y]==1){
                 k=y;
                 l=node;
-                //cout<<"\nFound a cycle\t"<<y<<endl;
                 return -1;
             }
-            visited[y]=true;
-            int i = recur(y, counter+1);
+            visited[y] = 1;
+            int i = recur(y,counter+1);
             if(i==-1){
                 return -1;
             }
-            visited[y]=false;
+            visited[y]= 0;
         }
-    }
-}
+      }
 
 
-void driver(){
-    for(int x=0; x<N; x++){
 
-        visited[x]=true;
-        path[0]=x;
-        //cout<<"\n\n***********************************\n"<<x;
-        for(int y=0; y<N; y++){
-            if(V[x][y]){
-                if(visited[y]){
-                    if(x==y){
-                        //Self loop found
-                        k=-1;
-                        return;
-                    }
-                }
-                visited[y]=true;
-                int ret = recur(y, 1);
-                if(ret==-1){
-                    return;
-                }
-                visited[y]=false;
-            }
+
+  }
+
+
+  void output(){
+     printf("\n");
+     if(k==-2){
+      printf("-1\n");return;//No cycle
+
+     }
+     if(k==-1){
+        printf("%d",path[0]);
+        printf("\n");
+         return;
+     }
+     int start_idx = 0;
+     for(int i=0;i<100;i++){
+        if(path[i]==k){
+            start_idx =i;
         }
+     }
 
-
-    }
-}
-
-
-void output(){
-    cout<<endl;
-    if(k==-2){
-        cout<<-1<<endl; //=> No cycle
-        return;
-    }
-    if(k==-1){
-        cout<<path[0]<<endl;
-        return;
-    }
-    int start_idx=0;
-    for(int i=0; i<100; i++){
-        if(path[i]==k)
-            start_idx=i;
-    }
-    for(int i=start_idx; i<100; i++){
+     for(int i=start_idx;i<100;i++){
         if(path[i]==l){
-            cout<<path[i]<<"->";
+            printf("%d",path[i]);
+            printf("->");
             break;
         }
-        cout<<path[i]<<"->";
-    }
-    cout<<path[start_idx];
-    cout<<endl;
-}
+         printf("%d",path[i]);
+        printf("->");
+     }
 
-int main(){
-    int test_cases;
-    cin>>test_cases;
-    while(test_cases>0){
-        test_cases--;
+     printf("%d",path[start_idx]);
+     printf("\n");
+
+   }
+
+
+
+  void driver(){
+        for(int x=0;x<N;x++){
+                visited[x] = 1;
+                path[0]=x;
+
+                for(int y=0;y<N;y++){
+                    if(V[x][y]==1){
+                            if(visited[y]==1){
+                                if(x==y){
+                                    k=-1;
+                                    return;
+                                }
+                            }
+                            visited[y] = 1;
+                            int ret = recur(y,1);
+                            if(ret==-1){
+                              return;
+                             }
+                            visited[y]=0;
+
+                }
+
+        }
+
+
+
+     }
+  }
+
+
+  int main(){
+
+    int test;
+    scanf("%d",&test);
+
+    while(test--){
         reset();
         input();
         driver();
         output();
+
     }
-}
+
+
+     return 0;
+
+
+  }
